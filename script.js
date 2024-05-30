4515047b9f0126101d578f30c2dd7dea42d9487d
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-05-30 20:12:52 +0400

    create element from js

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const books = [
-  {
-    title: "Red and Black",
-    janre: "romane",
-    grade: 4.23,
-  },
-  {
-    title: "Three MuSHketers",
-    janre: "romane",
-    grade: 4.22,
-  },
-  {
-    title: "Sherlock Holmse",
-    janre: "detective",
-    grade: 3.23,
-  },
-  {
-    title: "Clown",
-    janre: "adventure",
-    grade: 4.45,
-  },
-];
+// const books = [
+//   {
+//     title: "Red and Black",
+//     janre: "romane",
+//     grade: 4.23,
+//   },
+//   {
+//     title: "Three MuSHketers",
+//     janre: "romane",
+//     grade: 4.22,
+//   },
+//   {
+//     title: "Sherlock Holmse",
+//     janre: "detective",
+//     grade: 3.23,
+//   },
+//   {
+//     title: "Clown",
+//     janre: "adventure",
+//     grade: 4.45,
+//   },
+// ];
 
 // filter
 
@@ -34,16 +34,22 @@ const books = [
 
 // map
 
-let titles = books.map((book) => book.title);
-console.log(titles);
+// let titles = books.map((book) => book.title);
+// console.log(titles);
 
-// camelCase
+// // camelCase
 
-let camelize = (string) =>
-  string
-    .split("-")
-    .map((item) => item.charAt(0).toUpperCase() + item.slice(1))
-    .join("");
-console.log(camelize("background-color"));
-console.log(camelize("list-style-image"));
-console.log(camelize("-webkit-transition"));
+// let camelize = (string) =>
+//   string
+//     .split("-")
+//     .map((item) => item.charAt(0).toUpperCase() + item.slice(1))
+//     .join("");
+// console.log(camelize("background-color"));
+// console.log(camelize("list-style-image"));
+// console.log(camelize("-webkit-transition"));
+
+const link = document.createElement("p");
+link.innerText = "Hello Merabinho! ";
+link.style.backgroundColor = "green";
+link.style.color = "red";
+document.body.appendChild(link);
