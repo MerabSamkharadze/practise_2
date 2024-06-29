5ac1654a85fb5dbb52123bb1aa42a80a5e0a96e0
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-29 17:54:06 +0400

    add Color class

--- script.js
+++ script.js
@@ -169,9 +169,16 @@
 //     }
 //   });
 
-class Car {
-  constructorr(name, year) {
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
     this.name = name;
-    this.year = year;
+  }
+  rgb() {
+    const { r, g, b } = this;
+    return `rgb(${r},${g},${b})`;
   }
 }
+const m = new Color(255, 67, 89, "tomato");
